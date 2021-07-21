e2b1ff4bfc4b1f8f95ded6afd86bb4c3c32fa9ec
ArmaghanRazaChaudhary <67154539+ArmaghanRazaChaudhary@users.noreply.github.com>
2021-07-21 17:29:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,7 @@ const { NodeosActionReader } = require("demux-eos") // eslint-disable-line
 const ObjectActionHandler = require("./ObjectActionHandler")
 const handlerVersion = require("./handlerVersions/v1")
 
-try{
-
-  const uri = "mongodb://localhost:27017/demux";
-  const client = new MongoClient(uri);
-  client.connect();
-}
-catch(e){
-  console.error(e);
-}
-
 const actionHandler = new ObjectActionHandler([handlerVersion])
-
 const actionReader = new NodeosActionReader({
   startAtBlock: 130421054,
   onlyIrreversible: false,
