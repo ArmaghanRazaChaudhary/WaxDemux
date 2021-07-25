55ffea2d21653abe19f6fcdfcfa745363398e8c4
ArmaghanRazaChaudhary <67154539+ArmaghanRazaChaudhary@users.noreply.github.com>
2021-07-25 06:06:42 +0500

    Update api.js

--- api.js
+++ api.js
@@ -14,3 +14,33 @@ app.get('/getData', async function (req, res) {
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
+
+
+/*
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
+
+// your express configuration here
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
+
+
+
+
+
+
+openssl genrsa -out privatekey.pem 1024 
+openssl req -new -key privatekey.pem -out certrequest.csr 
+openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
+*/
